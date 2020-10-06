e8b550ddcb26ccfbaa57cebf6ca8a85a03e46ec9
K-SABARI-VISWANATH <72434967+K-SABARI-VISWANATH@users.noreply.github.com>
2020-10-06 23:33:11 +0530

    Add files via upload

---
+++ gatsby_cinemas/ghome.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>
+GATSBY CINEMAS - HOME
+</title>
+<link rel="stylesheet" type="text/css" href="ghome.css">
+</head>
+
+<body>
+	<div class="header">
+		<img src="logo.jpg">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="gabout.php">ABOUT</a></li>
+			<li><a href="gbooking.php">BOOKING</a></li>
+			<li><a href="gcontact.php">CONTACT</a></li>
+		</ul>
+	</div>
+	<div class="main1">
+	</div>
+	<div class="footer">
+		<p style="margin-top: 15px">
+		</p>
+	</div>
+	<div class="foot">
+		<p>
+			Copyright © 2020 Gatsby Cinemas. All Rights Reserved.
+		</p>
+	</div>
+</body>
+</html>
